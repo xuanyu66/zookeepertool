8022ae9351fe6c5bbcfced0ead4fd98c10cf04ba
Leon <angry_yang@163.com>
2018-07-31 00:24:02 +0800

    利用java反射原理撰写demo测试分布式队列

--- src/main/java/com/yangxin/distribute/demo/QueueDemo.java
+++ src/main/java/com/yangxin/distribute/demo/QueueDemo.java
@@ -2,6 +2,7 @@ package com.yangxin.distribute.demo;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.yangxin.distribute.queue.DistributeQueue;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
@@ -13,8 +14,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -30,25 +30,50 @@ public class QueueDemo {
     public static void main(String[] args) {
         ZooKeeper zooKeeper = connectServer();
         DistributeQueue queue = new DistributeQueue(zooKeeper, "/queue_demo", null);
-
-    }
-
-    private static byte[] invokeMethods(int field, String method, DistributeQueue queue){
-        byte[] data = new byte[0];
-        for (int i = 0; i < field; i++) {
-            LOGGER.info("{}", i);
+        Deque<byte[]> data = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
             Map<Long,String> map = new HashMap<>();
             map.put((long) i, Integer.toHexString(i));
-            data = GSON.toJson(map).getBytes();
+            LOGGER.debug("第{}个map的gson值 {}", i,GSON.toJson(map).getBytes());
+            data.add(GSON.toJson(map).getBytes());
+        }
+        Deque<byte[]> data2 = new LinkedList<>();
+        //invokeMethods(10, "offer", queue, data);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            LOGGER.error("sleep failed {}", e);
+        }
+        Map<Integer, byte[]> map = invokeMethods(10, "poll", queue, data2);
+        for (int i = 0; i < 10; i++) {
+            byte [] temp = map.get(i);
+            Map<Long,String> res = GSON.fromJson(new String(temp), new TypeToken<Map<Long,String>>(){}.getType());
+            LOGGER.info("得到的第{}个map {}", i, res);
+        }
+    }
+
+    private static Map<Integer, byte[]> invokeMethods(int field, String method, DistributeQueue queue, Deque<byte[]> data){
+        Map<Integer, byte[]> map = new HashMap<>();
+
+        for (int i = 0; i < field; i++) {
+            byte [] temp = data.pollFirst();
+            LOGGER.info("{}", i);
             try {
-                Method metho = queue.getClass().getMethod(method);
-                metho.invoke(queue, null);
-            } catch (InterruptedException | KeeperException | NoSuchMethodException |
+
+                if (temp == null){
+                    Method metho = queue.getClass().getMethod(method,  null);
+                    temp = (byte[]) metho.invoke(queue, null);
+                    map.put(i, temp);
+                }else {
+                    Method metho = queue.getClass().getMethod(method, temp.getClass());
+                    metho.invoke(queue, temp);
+                }
+            } catch ( NoSuchMethodException |
                     IllegalAccessException | InvocationTargetException e) {
                 LOGGER.error("{}", e);
             }
         }
-        return data;
+        return map;
     }
 
     private static ZooKeeper connectServer() {
